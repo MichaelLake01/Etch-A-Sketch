fb44e63261303f7da2d7b163fcbcbffbed5dd90a
MichaelLake01 <michael.lake.av18@gmail.com>
2023-02-12 17:59:25 +0000

    Improved mouse trail: varying grey shades, pixelated appearance, randomized spawning, and capped at 250 dots

--- app.js
+++ app.js
@@ -4,29 +4,74 @@ createGrid();
 const grids = document.querySelectorAll(".grid-box");
 
 const trail = document.createElement("div");
-defineTrail();
+addTrail();
 
-let height;
-let width;
+const trailColors = [
+    "#676F9F",
+    "#2E3131",
+    "#E8E8E8",
+    "#E8ECF1",
+    "#F3F1EF",
+    "#BFBFBF",
+    "#F2F1EF",
+    "#ABB7B7",
+    "#DADFE1",
+    "#9585A6",
+    "#ECF0F1",
+    "#BDC3C7",
+    "#EEE",
+    "#D2D7D3",
+    "#6C7A89",
+    "#ECECEC",
+];
+
 
 window.addEventListener("resize", updateSize);
 window.addEventListener("load", updateSize);
 
-function defineTrail()
+function addTrail()
 {
     trail.className = "trail";
     document.body.appendChild(trail);
 }
 
-let currentGridElement = null;
 
+function cleanTrail() {
+
+    const trailArr = Array.from(trail.children);
+    console.log(trailArr.length);
+    const maxAmount = 250;
+    while (trailArr.length > maxAmount)
+    {
+
+        let removedNode = trailArr.shift(); 
+        trail.removeChild(removedNode);
+    }
+
+}
+
+
+
+
+let currentGridElement = null;
 document.addEventListener("mousemove", function(event) {
     const dot = document.createElement("div");
+
+    const posX = event.clientX + (Math.random() * 20 - 10);
+    const posY = event.clientY + (Math.random() * 20 - 10);
+
     dot.className = "dot";
-    dot.style.left = event.clientX + "px";
-    dot.style.top = event.clientY + "px";
+    dot.style.left = posX + "px";
+    dot.style.top = posY + "px";
     trail.appendChild(dot);
-    
+
+    const range = (0.8 - 0.2) + 1;
+    const opacity = (0.2 + (Math.random() * range).toFixed(1));
+    dot.style.opacity = opacity;
+
+    trailColors.forEach(color => {
+        dot.style.backgroundColor = color;
+    });
     
     grids.forEach(e => {
         const rect = e.getBoundingClientRect();
@@ -39,6 +84,9 @@ document.addEventListener("mousemove", function(event) {
             }
         }
     });
+
+    cleanTrail();
+
 });
 
 
@@ -72,3 +120,24 @@ function getRandomColor() {
     randColor = randomNumber.padStart(6, 0);  
     return `#${randColor}`;
 }
+
+
+
+/*
+Hoki : – rgba(103, 128, 159, 1)
+Outer Space : – rgba(46, 49, 49, 1)
+Mercury : – rgba(232, 232, 232, 1)
+Mystic : – rgba(232, 236, 241, 1)
+Pampas : – rgba(243, 241, 239, 1)
+Silver : – rgba(191, 191, 191, 1)
+Cararra : – rgba(242, 241, 239, 1)
+Edward : – rgba(171, 183, 183, 1)
+Iron : – rgba(218, 223, 225, 1)
+Cascade : – rgba(149, 165, 166, 1)
+Porcelain : – rgba(236, 240, 241, 1)
+Silver Sand : – rgba(189, 195, 199, 1)
+Gallery : – rgba(238, 238, 238, 1)
+Pumice : – rgba(210, 215, 211, 1)
+Lynch : – rgba(108, 122, 137, 1)
+White Smoke : – rgba(236, 236, 236, 1)
+*/
