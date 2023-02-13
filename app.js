56b3ef14fbd10db311f73ca8d0a98998b8628fcd
MichaelLake01 <michael.lake.av18@gmail.com>
2023-02-13 13:39:57 +0000

    Implemented static placement for grid-container, enhancing functionality of grid layout

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const gridContainer = document.getElementById('grid-container');
+const gridContainer = document.querySelector('#grid-container');
 const trail = document.createElement("div");
 const button = document.querySelector("#grid-btn");
+const fullViewportWidth = window.innerWidth;
+
+let gridX = 16;
+let gridY = 16;
 let grids;
 
 button.addEventListener("click", function() {
-    const regex = /^([1-9]|[1-9][0-9]|100)x([1-9]|[1-9][0-9]|100)$/;
+    const regex = /^([1-9]|[1-9][0-9]|100)x([1-9]|[1 -9][0-9]|100)$/;
 
     let userInput = prompt("Enter a value in the format of 'XxY', where X and Y are numbers in the range of 1 to 100");
 
     if (regex.test(userInput)) {
         let numbers = userInput.split("x");
-        let num1 = parseInt(numbers[0]);
-        let num2 = parseInt(numbers[1]);
+        gridX = parseInt(numbers[0]);
+        gridY = parseInt(numbers[1]);
         removeGrids();
-        createGrid(num1, num2);  
+        innitGrid(gridX, gridY);  
+        
     }
     else 
     {
@@ -26,32 +31,79 @@ button.addEventListener("click", function() {
 });
 
 
-innitGrid();
-addTrail();
+
+   
+innitGrid(gridX,gridY);
 
 
-const trailColors = [
-    "#676F9F",
-    "#2E3131",
-    "#E8E8E8",
-    "#E8ECF1",
-    "#F3F1EF",
-    "#BFBFBF",
-    "#F2F1EF",
-    "#ABB7B7",
-    "#DADFE1",
-    "#9585A6",
-    "#ECF0F1",
-    "#BDC3C7",
-    "#EEE",
-    "#D2D7D3",
-    "#6C7A89",
-    "#ECECEC",
-];
+
+//addTrail();
+
+window.addEventListener("resize", function(){
+    updateGridContainerSize();
+    setGridBoxSize(updateGridBoxSize(gridX,gridY));
+
+});
 
 
-window.addEventListener("resize", updateSize);
+function innitGrid(x,y)
+{
+    for (let i = 0; i < x*y; i++) {
+        let grid = document.createElement('div');
+        grid.className = "grid-box";
+        gridContainer.appendChild(grid);
+    }
 
+    updateGridContainerSize();
+    grids = document.querySelectorAll(".grid-box");
+    setGridBoxSize(updateGridBoxSize(x,y));
+
+}
+
+function updateGridContainerSize()
+{
+    //const currentViewPort = window.innerWidth;
+    const maxValue = 800;
+    let size = maxValue;
+
+    /*if(currentViewPort === fullViewportWidth)
+    { 
+        size = maxValue;
+    }
+    else
+    {
+        size = maxValue - (currentViewPort- maxValue);
+    }*/
+  
+    gridContainer.style.width = `${size}px`;
+    gridContainer.style.height = `${size}px`;
+  
+}
+
+function setGridBoxSize(bW,bH)
+{
+    grids.forEach(e => {
+        e.style.width = `${bW}px`;
+        e.style.height = `${bH}px`;
+    });
+}
+
+function updateGridBoxSize(x,y)
+{
+    
+    const boxWidth = gridContainer.clientWidth / x;
+    const boxHeight = gridContainer.clientHeight / y;
+    return boxWidth, boxHeight;
+
+}
+
+
+function removeGrids()
+{
+    grids.forEach(e => {
+        e.remove();
+    });
+}
 
 function addTrail()
 {
@@ -60,54 +112,24 @@ function addTrail()
 }
 
 
-function cleanTrail() {
-
-    const trailArr = Array.from(trail.children);
-    const maxAmount = 175;
-    while (trailArr.length > maxAmount)
-    {
-
-        let removedNode = trailArr.shift(); 
-        trail.removeChild(removedNode);
-    }
-
-}
-
 let currentGridElement = null;
 document.addEventListener("mousemove", function(event) {
 
     if(event.target !== button)
     {
         let dot = document.createElement("div");   
-
-        const posX = event.clientX + (Math.random() * 20 - 10);
-        const posY = event.clientY + (Math.random() * 20 - 10);
     
         dot.className = "dot";
-        dot.style.left = posX + "px";
-        dot.style.top = posY + "px";
+        dot.style.left = event.clientX + "px";
+        dot.style.top = event.clientY + "px";
         trail.appendChild(dot);
     
-        changeTrailVals(dot);
         changeBoxColor(event);
     
-        cleanTrail();
     }
 
 });
 
-
-function changeTrailVals(dot){
-
-    const range = (0.8 - 0.2) + 1;
-    const opacity = (0.2 + (Math.random() * range).toFixed(1));
-    dot.style.opacity = opacity;
-
-    trailColors.forEach(color => {
-        dot.style.backgroundColor = color;
-    });
-
-}
 
 function changeBoxColor(event)
 {
@@ -117,90 +139,9 @@ function changeBoxColor(event)
             event.clientY >= rect.top && event.clientY <= rect.bottom) {
             
             if (e !== currentGridElement) {
-                e.style.backgroundColor = getRandomColor();
+                e.style.backgroundColor = "grey";
                 currentGridElement = e;
             }
         }
     });
 }
-
-function updateSize(x,y)
-{
-
-    height = gridContainer.clientHeight / y;
-    width = gridContainer.clientWidth / x;
-
-    
-    grids.forEach(e => {
-        e.style.width = `${width}px`;
-        e.style.height = `${height}px`;
-    });
- 
-}
-
-function removeGrids()
-{
-    grids.forEach(e => {
-        e.remove();
-    });
-}
-
-
-function createGrid(x,y){
-
-    let prevX;
-    let prevY;
-
-    if(prevX !== x && prevY !== y)
-    {     
-
-        prevX = x;
-        prevY = y;
-    
-        innitGrid(x,y);
-    }
-
-}
-
-function innitGrid(x = 16, y = 16)
-{
-    for (let i = 0; i < x*y; i++) {
-        let grid = document.createElement('div');
-        grid.className = "grid-box";
-        grid.style.backgroundColor = getRandomColor();
-        gridContainer.appendChild(grid);
-    }
-
-    grids = document.querySelectorAll(".grid-box");
-    updateSize(x,y);
-}
-
-function getRandomColor() {
-    let maxVal = 0xFFFFFF; // 16777215
-    let randomNumber = Math.random() * maxVal; 
-    randomNumber = Math.floor(randomNumber);
-    randomNumber = randomNumber.toString(16);
-    randColor = randomNumber.padStart(6, 0);  
-    return `#${randColor}`;
-}
-
-
-
-/*
-Hoki : – rgba(103, 128, 159, 1)
-Outer Space : – rgba(46, 49, 49, 1)
-Mercury : – rgba(232, 232, 232, 1)
-Mystic : – rgba(232, 236, 241, 1)
-Pampas : – rgba(243, 241, 239, 1)
-Silver : – rgba(191, 191, 191, 1)
-Cararra : – rgba(242, 241, 239, 1)
-Edward : – rgba(171, 183, 183, 1)
-Iron : – rgba(218, 223, 225, 1)
-Cascade : – rgba(149, 165, 166, 1)
-Porcelain : – rgba(236, 240, 241, 1)
-Silver Sand : – rgba(189, 195, 199, 1)
-Gallery : – rgba(238, 238, 238, 1)
-Pumice : – rgba(210, 215, 211, 1)
-Lynch : – rgba(108, 122, 137, 1)
-White Smoke : – rgba(236, 236, 236, 1)
-*/
